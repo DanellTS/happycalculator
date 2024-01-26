2886f22d7ed3d570ed7117b9f0a464c1519d1bd6
DanellTS <danell.ts@gmail.com>
2024-01-25 22:25:11 -0400

    sft calculator

--- WorstCalculator/ViewController.swift
+++ WorstCalculator/ViewController.swift
@@ -9,11 +9,53 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var firstValue: UITextField!
+    
+    @IBOutlet weak var secondValue: UITextField!
+
+    @IBOutlet weak var result: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
+    
+    func getValues() -> [String : Double] {
+        var data = [String : Double]()
+        var firstValueInt = Double(firstValue.text ?? "1")
+        var secondValueInt = Double(secondValue.text ?? "1")
+        
+        data["firstValue"] = firstValueInt
+        data["secondValue"] = secondValueInt
+        
+        return data;
+    }
+    
+    
+    @IBAction func sumBtn(_ sender: Any) {
+        var data = getValues();
+        result.text = String((data["firstValue"] ?? 1) + (data["secondValue"] ?? 1));
+        
+    }
+    
+    
+    @IBAction func subBtn(_ sender: Any) {
+        var data = getValues();
+        result.text = String((data["firstValue"] ?? 1) - (data["secondValue"] ?? 1));
+    }
+    
+    
+    @IBAction func mulBtn(_ sender: Any) {
+        var data = getValues();
+        result.text = String((data["firstValue"] ?? 1) * (data["secondValue"] ?? 1));
+    }
+    
+    
+    @IBAction func divBtn(_ sender: Any) {
+        var data = getValues();
+        result.text = String((data["firstValue"] ?? 1) / (data["secondValue"] ?? 1));
+    }
+    
 
 }
 
